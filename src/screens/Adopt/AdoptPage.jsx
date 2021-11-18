c880e80ef2d2366a7ac7d946de4230c40c0949c4
Sun, Tian <tsun90@gatech.edu>
2021-11-18 01:51:26 -0500

    adopt page added

--- src/screens/Adopt/AdoptPage.jsx
+++ src/screens/Adopt/AdoptPage.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import Router from "next/router";
-import { login, signUp } from "../../actions/User";
-import urls from "../../../utils/urls";
+import AnimalCard from '../../components/AnimalCard/AnimalCard'
+import style from './AdoptPage.module.css'
 
 export default function AdoptPage({animals}) {
 
   console.log(animals);
   return (
-    <h2>adopt</h2>
+    <div className={style.container}>
+      <h1> Browse Shelter Animals </h1>
+      <div className={style.list}>
+        {
+          animals && animals.map(animal => <AnimalCard animal={animal} key={animal.id}/>)
+        }
+      </div>
+    </div>
   )
 }

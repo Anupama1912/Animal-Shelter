7fefbe83807cf030a913b66d186e5cf0a361ce99
Bowie, Sophia Christine <sbowie7@gatech.edu>
2021-11-18 19:17:36 -0500

    fixed adopt form input requirements and css

--- src/screens/AdoptForm/AdoptForm.jsx
+++ src/screens/AdoptForm/AdoptForm.jsx
@@ -18,53 +18,82 @@ const AdoptRequest = async event => {
       },
       method: 'POST'
     })
+    event.target.name.value= "";
+    event.target.email.value= "";
+    event.target.Location.value= "";
+    event.target.phone.value= "";
+    event.target.description.value= "";
 
+    alert("Request recieved!");
     const result = await res.json()
   }
 
 export default function AdoptFormScreen( {animal} ) {
     let showMe = false
     return (
-        <div className = {style.form}>
-            <h3>Catscout</h3>
+      <div className = {style.body}>
+        <div className = {style.top}> 
+          <div className = {style.left}>
             <img src={animal.img} alt="" />
-                    <p>Name: {animal.name}</p>
-                    <p>Age: {animal.age}</p>
-                    <p>Location: {animal.location}</p>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada erat at 
-            bibendum maximus. Mauris id facilisis ligula. Donec et est lectus. Nulla facilisi. 
-            Proin rutrum scelerisque ante, eget interdum augue rutrum vel. Sed id ligula quis 
-            mauris laoreet suscipit. Sed luctus suscipit tortor, sit amet efficitur sapien dignissim eu. 
-            Sed consectetur ante at metus rhoncus, mattis commodo nunc egestas. Etiam eu pellentesque elit. 
-            Mauris commodo diam et nunc gravida, nec tempor diam ornare. Morbi nibh ligula, vestibulum ac 
-            massa quis, efficitur laoreet ligula. Sed ut purus sapien. Morbi sagittis felis porta, commodo
-             orci vel, auctor tellus. Donec sagittis maximus nisi quis tincidunt.
-            </p>
-            <form onSubmit = {AdoptRequest} className= {style.form}>
-                <div className = {style.item}>
-                    <label htmlFor="name">Name: </label>
-                    <input className = {style.marginc}  type="text" autoComplete="name" id="name" required/>
-                    <label className = {style.margina} htmlFor="email">Email: </label>
-                    <input className = {style.marginc} type="text" autoComplete="species" id="email" required/>
-                </div>
-                <br />
-                <div className = {style.item}>
-                    <label htmlFor="Location">Location: </label>
-                    <input type="text" name="" id="Location" required/>
-                    <label className = {style.marginb} htmlFor="phone" >Phone Number: </label>
-                    <input className = {style.marginc} type="text" name="" id="phone" required/>
-                </div>
-                <br />
-                <label htmlFor="description">Why do you want to adopt {animal.name}: </label>
-                <br />
-                <textarea name="description" id="description" cols="80" rows="10"></textarea>
-                {/* <input className = {style.description} type="text" name="" id="description" required/> */}
-                <br />
-                <input style={{display: showMe?"block":"none"}} type="text" id="which" name={animal.name}/>
-                <button type="submit">Submit</button>
-            </form>
+          </div>
+          <div className = {style.right}>
+              <p>Name: {animal.name}</p>
+              <p>Age: {animal.age}</p>
+              <p>Location: {animal.location}</p>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada erat at 
+              bibendum maximus. Mauris id facilisis ligula. Donec et est lectus. Nulla facilisi. 
+              Proin rutrum scelerisque ante, eget interdum augue rutrum vel. Sed id ligula quis 
+              mauris laoreet suscipit. Sed luctus suscipit tortor, sit amet efficitur sapien dignissim eu. 
+              Sed consectetur ante at metus rhoncus, mattis commodo nunc egestas. Etiam eu pellentesque elit. 
+              Mauris commodo diam et nunc gravida, nec tempor diam ornare. Morbi nibh ligula, vestibulum ac 
+              massa quis, efficitur laoreet ligula. Sed ut purus sapien. Morbi sagittis felis porta, commodo
+              orci vel, auctor tellus. Donec sagittis maximus nisi quis tincidunt.
+              </p>
+            </div>
         </div>
+        <div className={style.form}>
+          <h3> Adopt {animal.name} Today! </h3>
+          <form onSubmit = {AdoptRequest} className= {style.form}>
+            <div className = {style.item}>
+              <label htmlFor="name">Name: </label>
+              <input className = {style.marginc}  type="text" autoComplete="name" id="name" required/>
+              <label className = {style.margina}  htmlFor="email">Email: </label>
+              <input className = {style.marginc} type="email" autoComplete="species" id="email" required/>
+            </div>
+            <br />
+            <div className = {style.item}>
+              <label htmlFor="Location">Location: </label>
+              <input type="text" name="" id="Location" required/>
+              <label className = {style.marginb} htmlFor="phone" >Phone Number: </label>
+              <input className = {style.marginc} type="tel" id="phone" name="phone" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" required/>
+            </div>
+            <br />
+            <label htmlFor="description">Why do you want to adopt {animal.name}: </label>
+            <br />
+            <textarea name="description" id="description" cols="80" rows="10"></textarea>
+            {/* <input className = {style.description} type="text" name="" id="description" required/> */}
+            <br />
+            <input style={{display: showMe?"block":"none"}} type="text" id="which" name={animal.name}/>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+
+
+
+
+
+        // <div className = {style.form}>
+        //   <div className = {style.top}> 
+        //     <div className - {style.left}>
+              
+        //     </div>
+            
+        //     </div> 
+        //     </div>
+            
+        // </div>
         
 
     )
